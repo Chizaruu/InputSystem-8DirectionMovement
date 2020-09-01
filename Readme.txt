8132d0a6e9fe366b1b4097e8eaa11a3f97532330
ChizaruuGCO <AbdulCoskun@hotmail.com>
2020-09-01 11:08:38 +1000

    v1.2
    Repeated Movement is a thing now.

--- Readme.txt
+++ Readme.txt
@@ -3,8 +3,7 @@ SamYam's InputSystem modified for 8 Directional Grid Based Movement
 Changes-
 Hold Interaction instead of Press (Hold Time: 0.1)
 Can move 8 directions instead of 4 - I dont really want to explain the process (Vectors and stuff)
-
-
+FixedUpdate Method used for repeated movement.
 
 
 
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,9 +14,6 @@ public class PlayerController : MonoBehaviour
     
     //Input System
     private PlayerMovement controls;
-
-    //Cheeky Vector3
-    private Vector3 vec;
 
     private void Awake(){
         controls = new PlayerMovement();
@@ -31,21 +29,66 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        //Used for single turn by turn movement
+        //controls.Main.Movement.performed += _ => Move(); 
     }
 
-    private void Move(Vector2 direction){
-        vec = (Vector3)direction;
+
+    void FixedUpdate(){
+            //Used for repeated 8-direction movement
+            //PC, Linux, *Gasps* Mac
+            #if UNITY_STANDALONE 
+            //Arrowkeys
+            var up = Keyboard.current.upArrowKey.IsPressed();
+            var down = Keyboard.current.downArrowKey.IsPressed();
+            var left = Keyboard.current.leftArrowKey.IsPressed();
+            var right = Keyboard.current.rightArrowKey.IsPressed();
+
+            //Letterkeys
+            /*var w = Keyboard.current.wKey.IsPressed();
+            var s = Keyboard.current.sKey.IsPressed();
+            var a = Keyboard.current.aKey.IsPressed();
+            var d = Keyboard.current.dKey.IsPressed();*/
+
+            //Used to stop all movement if 3 arrowkeys are pressed at once.
+            if(up && down && left) return;
+            if(up && down && right) return;
+            if(up && left && right) return;
+            if(down && left && right) return;
+
+            //Used to stop all movement if 3 letterkeys are pressed at once.
+            /*if(w && s && a) return;
+            if(w && s && d) return;
+            if(w && a && d) return;
+            if(s && a && d) return;*/
+
+            //Player moves when an arrow key is pressed
+            if(up || down || right || left){
+                //Invoke because we want to give some time to read values if we want diagonal movement.
+                Invoke("Move", 0.1f);
+            }
+
+            //Player moves when an letter key is pressed
+            /*if(w || s || d || a){
+                //Invoke because we want to give some time to read values if we want diagonal movement.
+                Invoke("Move", 0.1f);
+            }*/
+            #endif
+        }
+
+    private void Move(){
+        Vector3 vec = (Vector3)controls.Main.Movement.ReadValue<Vector2>();
         vec = new Vector3(Mathf.Round(vec.x), Mathf.Round(vec.y), Mathf.Round(vec.z));
 
-        if (!CanMove(direction)) return;
+        Vector3 futurevec = transform.position + vec;
+        //Checks if you can move.
+        if (!CanMove(vec)) return;
+        //Checks if next movement will be same position.
+        if(futurevec == transform.position) return;
         transform.position += vec;
     }
 
-    private bool CanMove(Vector2 direction){
-        vec = (Vector3)direction;
-        vec = new Vector3(Mathf.Round(vec.x), Mathf.Round(vec.y), Mathf.Round(vec.z));
-
+    private bool CanMove(Vector3 vec){
         Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + vec);
         if (!groundTilemap.HasTile(gridPosition) || collisionTilemap.HasTile(gridPosition))
             return false;
