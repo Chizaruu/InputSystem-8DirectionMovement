7deff58b7bb572fd970255abedc45b3681d7611f
ChizaruuGCO <AbdulCoskun@hotmail.com>
2020-09-01 14:22:28 +1000

    Added more direction to PlayerController

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -33,48 +33,49 @@ public class PlayerController : MonoBehaviour
         //controls.Main.Movement.performed += _ => Move(); 
     }
 
-
+    // remove both // above and below FixedUpdate to change it to a comment and prevent repeated movement.
+    // /*
     void FixedUpdate(){
-            //Used for repeated 8-direction movement
-            //PC, Linux, *Gasps* Mac
-            #if UNITY_STANDALONE 
-            //Arrowkeys
-            var up = Keyboard.current.upArrowKey.IsPressed();
-            var down = Keyboard.current.downArrowKey.IsPressed();
-            var left = Keyboard.current.leftArrowKey.IsPressed();
-            var right = Keyboard.current.rightArrowKey.IsPressed();
+        //Used for repeated 8-direction movement
+        //PC, Linux, *Gasps* Mac
+        #if UNITY_STANDALONE 
+        //Arrowkeys
+        var up = Keyboard.current.upArrowKey.IsPressed();
+        var down = Keyboard.current.downArrowKey.IsPressed();
+        var left = Keyboard.current.leftArrowKey.IsPressed();
+        var right = Keyboard.current.rightArrowKey.IsPressed();
 
-            //Letterkeys
-            var w = Keyboard.current.wKey.IsPressed();
-            var s = Keyboard.current.sKey.IsPressed();
-            var a = Keyboard.current.aKey.IsPressed();
-            var d = Keyboard.current.dKey.IsPressed();
+        //Letterkeys
+        var w = Keyboard.current.wKey.IsPressed();
+        var s = Keyboard.current.sKey.IsPressed();
+        var a = Keyboard.current.aKey.IsPressed();
+        var d = Keyboard.current.dKey.IsPressed();
 
-            //Used to stop all movement if 3 arrowkeys are pressed at once.
-            if(up && down && left) return;
-            if(up && down && right) return;
-            if(up && left && right) return;
-            if(down && left && right) return;
+        //Used to stop all movement if 3 arrowkeys are pressed at once.
+        if(up && down && left) return;
+        if(up && down && right) return;
+        if(up && left && right) return;
+        if(down && left && right) return;
 
-            //Used to stop all movement if 3 letterkeys are pressed at once.
-            if(w && s && a) return;
-            if(w && s && d) return;
-            if(w && a && d) return;
-            if(s && a && d) return;
+        //Used to stop all movement if 3 letterkeys are pressed at once.
+        if(w && s && a) return;
+        if(w && s && d) return;
+        if(w && a && d) return;
+        if(s && a && d) return;
 
-            //Player moves when an arrow key is pressed
-            if(up || down || right || left){
-                //Invoke because we want to give some time to read values if we want diagonal movement.
-                Invoke("Move", 0.1f);
-            }
-
-            //Player moves when an letter key is pressed
-            if(w || s || d || a){
-                //Invoke because we want to give some time to read values if we want diagonal movement.
-                Invoke("Move", 0.1f);
-            }
-            #endif
+        //Player moves when an arrow key is pressed
+        if(up || down || right || left){
+            //Invoke because we want to give some time to read values if we want diagonal movement.
+            Invoke("Move", 0.1f);
         }
+
+        //Player moves when an letter key is pressed
+        if(w || s || d || a){
+            //Invoke because we want to give some time to read values if we want diagonal movement.
+            Invoke("Move", 0.1f);
+        }
+        #endif
+    } // */
 
     private void Move(){
         Vector3 vec = (Vector3)controls.Main.Movement.ReadValue<Vector2>();
